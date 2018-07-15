7423972d4c25a2a5b61da77df178f93ca8587d76
mhattingpete <petersen2630@gmail.com>
2018-07-15 23:25:43 +0200

    added results for GAN and corrections to discriminators

--- discriminators.py
+++ discriminators.py
@@ -8,26 +8,22 @@ class Discriminator(nn.Module):
 	def __init__(self,hidden_sizes,dropout_prob=0.1,activation=nn.ELU(),last_activation=nn.Sigmoid()):
 		super().__init__()
 		self.layers = MultiLayerPerceptron(hidden_sizes,activation=activation,dropout_prob=dropout_prob,last_activation=last_activation)
-		self.softmax = nn.LogSoftmax(dim=-1)
 
 	def forward(self,x):
-		x = self.layers(x)
-		return self.softmax(x)
+		return self.layers(x)
 
 class ConvDiscriminator(nn.Module):
-	def __init__(self,hidden_sizes,activation=nn.ELU()):
+	def __init__(self,hidden_sizes,activation=nn.ELU(),last_activation=nn.Sigmoid()):
 		super().__init__()
 		# layers
 		layers = []
 		for i in range(len(hidden_sizes)-1):
 			layers.append(nn.Conv2d(hidden_sizes[i],hidden_sizes[i+1],kernel_size=(3,3),stride=(1,1)))
 			if (i+1 == len(hidden_sizes)-1):
-				continue
+				layers.append(last_activation)
 			else:
 				layers.append(activation)
 		self.layers = nn.Sequential(*layers)
-		self.softmax = nn.LogSoftmax(dim=-1)
 
 	def forward(self,x):
-		x = self.layers(x)
-		return self.softmax(x)
+		return self.layers(x)
