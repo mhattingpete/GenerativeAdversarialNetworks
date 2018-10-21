3000d3fc1cf77973d82e9069ba3ad10b6b2dfa01
Martin Hatting Petersen <s144234@compute.dtu.dk>
2018-10-21 10:49:02 +0200

    added run model

--- visualize.py
+++ visualize.py
@@ -1,14 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plotSamples(x,xu=None,yu=1,max_print=100):
+def plotSamples(x,xu=None,yu=1,max_print=100,title="Data"):
 	x = x.cpu().data.numpy()
 	fig = plt.figure()
+	fig.suptitle(title)
 	n = int(np.floor(np.sqrt(max_print)))
 	nsq = n*n
+	if xu == None:
+		xu = x.shape[1]
 	for i in range(x.shape[0]):
-		if i == 1 and xu == None:
-			xu = x[i,:].shape[0]
 		if i >= nsq:
 			break
 		ax = fig.add_subplot(n,n,i+1)
